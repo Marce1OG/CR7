0585d68f521647cdee9d33c2cca419b3f4d6e7d3
Marce1OG <159509850+Marce1OG@users.noreply.github.com>
2024-11-20 06:03:00 +0100

    Delete Ronaldo-free-shirt directory

--- Ronaldo-free-shirt/ronaldo.js
+++
@@ -1,5 +0,0 @@
-// Function to redirect to the 'iamnotrobot.html' page
-function redirectToRobotPage() {
-  // Navigate to the "You Are Not a Robot" page
-  window.location.href = "iamnotrobot.html";
-}
